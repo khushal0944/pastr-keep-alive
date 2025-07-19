373d681d74b02edfbcb488d8fd8d0eda5a6885db
Khushal Kumar <146096972+khushal0944@users.noreply.github.com>
2025-07-19 14:44:23 +0530

    added timestamp

--- ping.js
+++ ping.js
@@ -2,10 +2,12 @@ const https = require("https");
 
 const url = "https://online-clipboard-99xm.onrender.com/check"; // Replace with your backend URL
 
+const timestamp = new Date().toISOString();
+
 https
 	.get(url, (res) => {
-		console.log(`Pinged: ${url}, Status: ${res.statusCode}`);
+		console.log(`[${timestamp}] Pinged: ${url}, Status: ${res.statusCode}`);
 	})
 	.on("error", (e) => {
-		console.error(`Error: ${e.message}`);
+		console.error(`[${timestamp}] Error: ${e.message}`);
 	});
